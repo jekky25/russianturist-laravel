c731f6e37376a53a5a9dacbffaf455bc3c4a3ef6
jekky25 <jekky25@list.ru>
2024-06-05 03:39:53 +0300

    add comment

--- app/Http/Middleware/DontEndSlash.php
+++ app/Http/Middleware/DontEndSlash.php
@@ -32,6 +32,12 @@ class DontEndSlash
 		return $next($request);
 	}
 
+	/**
+	* get uri by request
+	*
+	* @param  \Illuminate\Http\Request  $request
+	* @return string
+	*/
 	public function getUri($request)
 	{
 		$uri = $request->getRequestUri();
