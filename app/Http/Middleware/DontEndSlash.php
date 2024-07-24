1c4cbc8057f834cb82ec277c329599a821e1572c
jekky25 <jekky25@list.ru>
2024-07-24 09:39:34 +0300

    fix bugs with the pagination

--- app/Http/Middleware/DontEndSlash.php
+++ app/Http/Middleware/DontEndSlash.php
@@ -10,23 +10,33 @@ use Config;
 
 class DontEndSlash
 {
-  /**
-  * Handle an incoming request.
-  *
-  * @param  \Illuminate\Http\Request  $request
-  * @param  \Closure  $next
-  * @return mixed
-  */
-  public function handle($request, Closure $next)
-  {
+	private $checkPattern		= '/.+?page=([0-9]+)\/$/';
+	private $redirectPattern	= '/.([0-9A-Za-z]*)\?page=([0-9]+)\/$/';
+	private $replacementPattern	= '$1/?page=$2';
+
+	/**
+	* Handle an incoming request.
+	*
+	* @param  \Illuminate\Http\Request  $request
+	* @param  \Closure  $next
+	* @return mixed
+	*/
+	public function handle($request, Closure $next)
+	{
 		//do it for phpUnit tests
-		$requestUri = $this->getUri($request);
-		$trailingSlash 	= (Str::contains($requestUri, ['#', '.html', '/?']) ? '' : '/');
+		$requestUri		= $this->getUri($request);
+		$trailingSlash	= (Str::contains($requestUri, ['#', '.html', '/?']) ? '' : '/');
+		$base_url		= Config::get('app.url');
+
+		if ($this->checkUriForRedirect($requestUri))
+		{
+			$requestReplaceUri = $this->getNewUri($requestUri);
+			return Redirect::to($base_url.$trailingSlash.$requestReplaceUri);
+		}
 
 		//check url for the presence of a trailing slash
 		if (!empty($trailingSlash) && !preg_match('/.+\/$/', $requestUri))
 		{
-			$base_url = Config::get('app.url');
 			return Redirect::to($base_url.$request->getRequestUri().$trailingSlash);
 		}
 		return $next($request);
@@ -42,5 +52,27 @@ class DontEndSlash
 	{
 		$uri = $request->getRequestUri();
 		return $uri != $_SERVER['REQUEST_URI'] && !empty($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : $uri;
+	}
+
+	/**
+	* check uri if we get pages like item?page=2/ to redirect the to the valid url
+	*
+	* @param string $requestUri
+	* @return bool
+	*/
+	public function checkUriForRedirect($requestUri)
+	{
+		return preg_match($this->checkPattern, $requestUri);
+	}
+
+	/**
+	* get a new uri for redirect
+	*
+	* @param  string $requestUri
+	* @return string
+	*/
+	public function getNewUri($requestUri)
+	{
+		return preg_replace($this->redirectPattern, $this->replacementPattern, $requestUri);
 	}
 }
