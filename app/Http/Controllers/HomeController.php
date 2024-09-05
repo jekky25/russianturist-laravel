c17ee1212749887d71d89209b39fd906660e97f5
jekky25 <jekky25@list.ru>
2024-09-05 07:49:16 +0300

    refactoring

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,33 +1,22 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Services\HotelService;
 use App\Services\CountryService;
-
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Contracts\Auth\Authenticatable;
 use App\Traits\BaseConfig;
-
-use App\Models\User;
-use App\Models\Hotel;
-use App\Models\Foto;
-use App\Models\Country;
 
 class HomeController extends Controller
 {
 	use BaseConfig;
 	public $boardingConfig 	= [];
 	private $countHotels 	= 5;
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
+	/**
+	* Create a new controller instance.
+	*
+	* @return void
+	*/
 	public function __construct(
 		public CountryService $countryService,
 		public HotelService $hotelService
@@ -36,19 +25,16 @@ class HomeController extends Controller
 		$this->boardConfig = $this->getBoardConfig();
 	}
 
-    /**
-     * Show the application dashboard.
-	 * 
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-	public function index(Request $request)
+	/**
+	* Show the application dashboard.
+	* 
+	* @return \Illuminate\Http\Response
+	*/
+	public function index()
 	{
 		$hotels			= $this->hotelService->getByLimit($this->countHotels);
 		$countries		= $this->countryService->getAll();
 		
-		$arMeta = [];
-
 		$title 		= 'Страны, русский турист, сайт про туризм и путешествия';
 		$arMeta = [
 			'title' => $title
