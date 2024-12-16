4584e74d4405f860c3d19a6af25905b037b6ee99
jekky25 <jekky25@list.ru>
2024-12-16 08:43:03 +0300

    add middlewares to the admin route

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function() {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'admin']], function() {
 	Route::group(['namespace' => 'Main'], function() {
 		Route::get('/', 'IndexController');
 	});
