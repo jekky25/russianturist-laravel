0da6bd6e3746425d316a7ec65f5f2e42ddd32533
jekky25 <jekky25@list.ru>
2025-01-07 09:19:52 +0300

    fix the old bug in the css and move it to the separate media

--- resources/views/layouts/app_spa.blade.php
+++ resources/views/layouts/app_spa.blade.php
@@ -6,8 +6,10 @@
 <meta http-equiv="Content-Style-Type" content="text/css">
 <meta name="yandex-verification" content="fe6c88b999494404" />
 <meta content="Туризм и путешествия" name="classification">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?/*=$CODE_INPUT['META']*/?>
-<link href="{{ asset("css/site.css?t=10") }}" type="text/css" rel="stylesheet">
+<link href="{{ asset("css/site.css?t=11") }}" type="text/css" rel="stylesheet">
+<link href="{{ asset("css/media.css?t=12") }}" type="text/css" rel="stylesheet">
 </head>
 <body>
 <div id="app" class="overhtm">
